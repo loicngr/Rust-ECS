8551d6ca90ed14003a825890269d1e9dae86f083
loicngr <pro.nogierloic@gmail.com>
2020-08-23 16:13:58 +0200

    Update

--- src/entity/mod.rs
+++
@@ -1,5 +0,0 @@
-use component::Component;
-
-pub struct Character<T> {
-    pub components: Vec<Component::<T>>,
-}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,62 @@
-use entity::Character;
-use component::{Position, Component, Inventory, Slot};
+use std::any::Any;
+use std::fmt::Debug;
 
-mod component;
-mod entity;
-mod system;
+trait Component {}
+trait Entity {
+    fn add_component<T: 'static + Component>(&mut self, component: T);
+}
+
+#[derive(Clone)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+impl Component for Position {}
+
+#[derive(Clone)]
+struct Inventory {
+    slots: Vec<i32>
+}
+impl Component for Inventory {}
+
+
+struct Character {
+    components: Vec<Box<dyn Component>>
+}
+
+struct House {
+    components: Vec<Box<dyn Component>>
+}
+
+impl Entity for Character {
+    fn add_component<T: 'static + Component>(&mut self, component: T) {
+        self.components.push(Box::new(component))
+    }
+}
+impl Entity for House {
+    fn add_component<T: 'static + Component>(&mut self, component: T) {
+        self.components.push(Box::new(component))
+    }
+}
 
 fn main() {
-    let mut e_character = Character { components: vec![] }; // Entité character
     
-    let mut c_position = Position { x: 10, y: 50 }; // Composant de position
-    
-    e_character.components.push(Component::<Position>(c_position) ); // Ajout du composant de position dans l'entité character
+    // Player entity
+    let mut entity_character = Character { components: vec![] };
 
-    let mut c_inventory = Inventory { slots: vec![] };
-    c_inventory.slots.push(Slot { id: 1, name: "Book", order: 1 });
+    // House entity
+    let mut entity_house = House { components: vec![] };
 
-    e_character.components.push(Component::<Inventory>(c_inventory));
+    // Position Component
+    let mut component_position = Position { x: 0, y: 0 };
+
+    // Inventory Component
+    let mut component_inventory = Inventory { slots: vec![] };
+
+    entity_character.add_component(component_position.clone());
+    entity_character.add_component(component_inventory.clone());
+
+    entity_house.add_component(component_position.clone());
+
+    println!("ok");
 }
